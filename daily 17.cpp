b992dd215e0a85b0fddbbaf19bc91fd70f8915cb
beautybin <2953510065@qq.com>
2023-01-18 17:38:42 +0800

    指针2

---
+++ daily 17.cpp
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+	///*int* p;
+	//*p = 20;*///非法访问内存
+	//int arr[10] = { 0 };
+	//int* p = arr;
+	//int i = 0;
+	//for (i = 0; i <= 10; i++)
+	//{
+	//	*p = i;
+	//	p++;
+	//}//越界访问，造成野指针;
+	//int* test()
+	//{
+	//	int a = 10;
+	//	return &a;
+	//}
+	//int main()
+	//{
+	//	int* p = test();
+	//	*p = 20;
+	//}
+	////指针指向了空间释放;
+	return 0;
+}
