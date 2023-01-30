36375f7877ee99a9cb7542fcd88337d3e4270512
beautybin <2953510065@qq.com>
2023-01-30 12:28:12 +0800

    Create daily study 17.cpp

---
+++ daily study 17.cpp
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+	//int arr[10] = { 0 };
+	//printf("%p\n", arr);//数组名时数组首元素地址
+	//printf("%p\n", &arr[0]);
+	//int arr[10] = { 0 };
+	//int* p = arr;
+	//int i = 0;
+	//for (i = 0; i < 10; i++)
+	//{
+	//	*(p + i)=i;//p+i表示数组内第i个元素
+	//}
+	//for (i = 0; i < 10; i++)
+	//{
+	//	printf("%d", *(p + i));
+	//}
+	int* arr[5];//指针数组，存放指针的数组，int*arr[i]表示存放整形指针，其他指针数组以此类推
+	return 0;
+}
